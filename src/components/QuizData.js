fcfe11a56ea59eda88ea2e340d3c3b1bd817115a
Pau Bonilla <paulo111995@gmail.com>
2020-07-18 16:19:23 -0700

    added new file

---
+++ src/components/QuizData.js
@@ -0,0 +1,130 @@
+export const QuizData = [
+    {
+        id: 0,
+        question: `You should increase the distance between your vehicle and the vehicle ahead when you:`,
+        options: [`Are following a small passenger vehicle.`, `Are being tailgated by another driver.`, `Are driving more slowly than the posted speed limit.`],
+        answer: `Are being tailgated by another driver.`
+    },
+    {
+        id: 1,
+        question: `In which of the following scenarios should your wheels not be pointed straight ahead?`,
+        options: [`When waiting to make a left turn at a traffic light.`, `When parked on a hill or sloping driveway.`, `When parked on the side of a level roadway where there is no curb.`],
+        answer: `When parked on a hill or sloping driveway.`
+    },
+    {
+        id: 2,
+        question: `If a truck or bus is making a right turn where you also need to make a right turn, you should:`,
+        options: [`Quickly turn before the truck or bus is able to.`, `Wait until the truck or bus turns before you turn.`, `Squeeze between the truck or bus and the curb.`],
+        answer: `Wait until the truck or bus turns before you turn.`
+    },
+    {
+        id: 3,
+        question: `
+        When being followed by a tailgater, which of the following will help you avoid being hit from behind?`,
+        options: [`Merging into another lane`, `Decreasing your following distance`, `Changing lanes frequently`],
+        answer: `Decreasing your following distance`
+    },
+    {
+        id: 4,
+        question: `When you see this sign, you:`,
+        options: [`Are approaching a railroad crossing and should prepare to stop.`, `Will always stop at the upcoming railroad crossing.`, `Should stop and wait for a signal before crossing the railroad tracks.`],
+        answer: `Are approaching a railroad crossing and should prepare to stop.`
+    },
+    {
+        id: 5,
+        question: `When driving under snowy or icy conditions:`,
+        options: [`It is safe to use your cruise control.`, `Make speed and directional changes more gradually than you would otherwise.`, `Drive as you would under normal conditions.`],
+        answer: `Make speed and directional changes more gradually than you would otherwise.`
+    },
+    {
+        id: 6,
+        question: `A broken yellow centerline means that:`,
+        options: [`Passing is not permitted.`, `Passing on the right is permitted when the way ahead is clear.`, `Passing on the left is permitted when the way ahead is clear.`],
+        answer: `Passing on the left is permitted when the way ahead is clear.`
+    },
+    {
+        id: 7,
+        question: `When driving in fog, rain, or snow, use:`,
+        options: [`Low beams.`, `High beams.`, `Fog lights only.`],
+        answer: `Low beams.`
+    },
+    {
+        id: 8,
+        question: `
+        Which of these statements is true about changing lanes?`,
+        options: [`You only need to turn and look over your right shoulder for lane changes to the right or left.`, `Look over your right shoulder for a right lane change and your left shoulder for a left lane change.`, `Vehicles with two outside mirrors do not have blind spots.`],
+        answer: `Look over your right shoulder for a right lane change and your left shoulder for a left lane change.`
+    },
+    {
+        id: 9,
+        question: `It is illegal for a person 21 years of age or older to drive with a blood alcohol concentration (BAC) that is ___ or higher...`,
+        options: [`.08%`, `.10%`, `.05%`],
+        answer: `.08%`
+    },
+    {
+        id: 10,
+        question: `You must notify the DMV within 5 days if..`,
+        options: [`Sell or transfer your vehicle`, `Are cited for traffic violation`, `Paint your vehicle a different color`],
+        answer: `Sell or transfer your vehicle`
+    },
+    {
+        id: 11,
+        question: `At dawn or dusk, or in rain or snow, it can be hard to see and be seen. A good way to let other drivers know you are there is to turn...`,
+        options: [`Up the instrument panel lights`, `On your parking lights`, `On your headlights`],
+        answer: `On your headlights`
+    },
+    {
+        id: 12,
+        question: `When changing lanes on a freeway, you should..`,
+        options: [`Signal for at least 5 seconds`, `Slow down before you start to change lanes`, `Assume there is enough space for your vehicle, if you signal first`],
+        answer: `Assume there is enough space for your vehicle, if you signal first`
+    },
+    {
+        id: 13,
+        question: ``,
+        options: [``],
+        answer: ``
+    },
+    {
+        id: 14,
+        question: ``,
+        options: [``],
+        answer: ``
+    },
+    {
+        id: 15,
+        question: ``,
+        options: [``],
+        answer: ``
+    },
+    {
+        id: 16,
+        question: ``,
+        options: [``],
+        answer: ``
+    },
+    {
+        id: 17,
+        question: ``,
+        options: [``],
+        answer: ``
+    },
+    {
+        id: 18,
+        question: ``,
+        options: [``],
+        answer: ``
+    },
+    {
+        id: 19,
+        question: ``,
+        options: [``],
+        answer: ``
+    },
+    {
+        id: 20,
+        question: ``,
+        options: [``],
+        answer: ``
+    },
+]
