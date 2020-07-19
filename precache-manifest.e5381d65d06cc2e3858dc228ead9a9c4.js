1292668235f4a70e6e0f2c9fb14838cbe97924f1
Pau Bonilla <paulo111995@gmail.com>
2020-07-19 16:41:59 -0700

    Updates

---
+++ precache-manifest.e5381d65d06cc2e3858dc228ead9a9c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1dfa836e884a7d0fac398f04870d427",
+    "url": "/dmv-test/index.html"
+  },
+  {
+    "revision": "c226daa09de8a86f4756",
+    "url": "/dmv-test/static/css/main.756de27d.chunk.css"
+  },
+  {
+    "revision": "13e768d99f3841226b24",
+    "url": "/dmv-test/static/js/2.d483316f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dmv-test/static/js/2.d483316f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c226daa09de8a86f4756",
+    "url": "/dmv-test/static/js/main.6055b850.chunk.js"
+  },
+  {
+    "revision": "8359abeaf6091ea03f5c",
+    "url": "/dmv-test/static/js/runtime-main.1481d302.js"
+  }
+]);
